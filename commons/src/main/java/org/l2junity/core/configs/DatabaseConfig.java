7a9c7b582a9bc35c9d3f7185f40e1164aad6df4b
Anton Lasevich <AN3Orik@users.noreply.github.com>
2017-04-02 21:45:19 +0000

    ?????? ??? ?3p0 ?? Hikari (???? ??? ???????? ??????????)

--- commons/src/main/java/org/l2junity/core/configs/DatabaseConfig.java
+++ commons/src/main/java/org/l2junity/core/configs/DatabaseConfig.java
@@ -4,22 +4,26 @@ import org.l2junity.commons.config.annotation.ConfigComments;
 import org.l2junity.commons.config.annotation.ConfigFile;
 import org.l2junity.commons.config.annotation.ConfigProperty;
 
+import java.sql.Driver;
+
 /**
  * @author ANZO
  */
 @ConfigFile(name = "configs/database.properties")
 public class DatabaseConfig {
-	@ConfigProperty(name = "URL", value = "jdbc:mysql://localhost/l2jgs?useSSL=false&serverTimezone=Europe/Paris")
+	@ConfigProperty(name = "URL", value = "jdbc:mysql://localhost/l2jgs?useUnicode=true&characterEncoding=utf-8")
 	@ConfigComments(comment = {
 			"Database URL",
-			"URL = jdbc:mysql://localhost/l2jgs?useSSL=false&serverTimezone=Europe/Paris (default)",
-			"URL = jdbc:hsqldb:hsql://localhost/l2jgs",
-			"URL = jdbc:sqlserver://localhost/database = l2jgs/user = sa/password = "})
+			"URL = jdbc:mysql://localhost/l2jgs?useUnicode=true&characterEncoding=utf-8"})
 	public static String DATABASE_URL;
 
 	@ConfigProperty(name = "Login", value = "root")
 	@ConfigComments(comment = "Database user info (default is \"root\" but it's not recommended).")
 	public static String DATABASE_LOGIN;
+
+	@ConfigProperty(name = "Driver", value = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource")
+	@ConfigComments(comment = {"SQL Driver", "Default: com.mysql.jdbc.jdbc2.optional.MysqlDataSource"})
+	public static String DRIVER;
 
 	@ConfigProperty(name = "Password", value = "")
 	@ConfigComments(comment = "Database connection password.")
@@ -27,11 +31,4 @@ public class DatabaseConfig {
 
 	@ConfigProperty(name = "MaximumDbConnections", value = "100")
 	public static int DATABASE_MAX_CONNECTIONS;
-
-	@ConfigProperty(name = "MaximumDbIdleTime", value = "0")
-	public static int DATABASE_MAX_IDLE_TIME;
-
-	@ConfigProperty(name = "ConnectionCloseTime", value = "60000")
-	@ConfigComments(comment = "Connection close time.")
-	public static long CONNECTION_CLOSE_TIME;
 }
