a8af03ceb5261d9770266b6b32c07d44c47717d6
hack <Hackness@users.noreply.github.com>
2017-04-03 06:55:08 +0000

    ?????????? ??? ?????? ??????

---
+++ gameserver/src/main/java/org/l2junity/gameserver/data/xml/impl/CustomMessageData.java
@@ -0,0 +1,39 @@
+package org.l2junity.gameserver.data.xml.impl;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.l2junity.core.startup.StartupComponent;
+import org.l2junity.gameserver.data.xml.IGameXmlReader;
+import org.l2junity.gameserver.network.client.send.string.CustomMessage;
+import org.w3c.dom.Document;
+
+import java.nio.file.Path;
+
+/**
+ * Created by Hack
+ * Date: 03.04.2017 7:57
+ */
+@Slf4j
+@StartupComponent(value = "Data")
+public class CustomMessageData implements IGameXmlReader {
+    @Getter(lazy = true)
+    private static final CustomMessageData instance = new CustomMessageData();
+
+    CustomMessageData() {
+        reload();
+    }
+
+    private void reload() {
+        parseDatapackFile("data/lang/custom_messages.xml");
+    }
+
+    @Override
+    public void parseDocument(Document doc, Path path) {
+        forEach(doc, "list", listNode -> forEach(listNode, "message", msgNode -> {
+            CustomMessage message = CustomMessage.valueOf(parseString(msgNode.getAttributes(), "name"));
+            forEach(msgNode, "lang", lang -> {
+                message.addMessage(parseString(lang.getAttributes(), "name"), lang.getTextContent());
+            });
+        }));
+    }
+}
